6a574863057e04d8925e8a8105ba2e81364d4797
Ernesto <jorge.cywdt@gmail.com>
2023-04-17 14:28:34 -0500

    Notas para entender mejor el codigo

--- curd/models.py
+++ curd/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Emp(models.Model):
+
+class Emp(models.Model):  # Modelo creado para ejecutar migraciones
+    # Definir los datos de la clase (campos de la tabla)
+    # Si no se especifica lo contrario, todos los campos seran requeridos por defecto, es decir NOT NULL
     emp_name = models.TextField()
     emp_email = models.EmailField()
     emp_mobile = models.TextField()
